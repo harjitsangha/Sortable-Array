65d8d5da5dcd42cf6227e32f24ffd7b1d5acf9cc
harjitsangha <harjitsangha41@outlook.com>
2024-10-27 11:15:21 -0700

    Add integer sorting functionality

--- src/SortableArray.java
+++ src/SortableArray.java
@@ -1,1 +1,22 @@
 
+import java.util.Arrays;
+
+/**
+ * Utility class to sort arrays of integers and strings.
+ */
+public class SortableArray {
+
+    /**
+     * Sorts an array of integers in ascending order.
+     * @param array The array of integers to be sorted.
+     */
+    public static void sortIntegers(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void main(String[] args) {
+        int[] integers = {5, 3, 8, 1, 2};
+        sortIntegers(integers);
+        System.out.println("Sorted integers: " + Arrays.toString(integers));
+    }
+}
